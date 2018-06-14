c57e7991f33c54474c358f292d534fb295814b83
Thiago Souza <souzat1985@outlook.com>
2018-06-14 00:27:47 -0300

    Adcionado views

---
+++ eGaragem.UI.Mvc/Controllers/LocadorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eGaragem.UI.Mvc.Controllers
+{
+    public class LocadorController : Controller
+    {
+        // GET: Locador
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- eGaragem.UI.Mvc/Models/ManageViewModels.cs
+++ eGaragem.UI.Mvc/Models/ManageViewModels.cs
@@ -28,14 +28,14 @@ namespace eGaragem.UI.Mvc.Models
     public class SetPasswordViewModel
     {
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "O {0} deve ser pelo menos {2} caracteres de comprimento.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Compare("NewPassword", ErrorMessage = "A nova senha e a senha de confirmação não são iguais.")]
         public string ConfirmPassword { get; set; }
     }
 
@@ -47,14 +47,14 @@ namespace eGaragem.UI.Mvc.Models
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "O {0} deve ser pelo menos {2} caracteres de comprimento.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Compare("NewPassword", ErrorMessage = "A nova senha e a senha de confirmação não são iguais.")]
         public string ConfirmPassword { get; set; }
     }
 
