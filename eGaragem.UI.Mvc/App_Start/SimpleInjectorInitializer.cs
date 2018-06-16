1e689a306950b726a1633c3f5aba2adb9d439926
Thiago Souza <souzat1985@outlook.com>
2018-06-16 00:50:19 -0300

    Adicionado simpleinjector startup

--- eGaragem.UI.Mvc/App_Start/SimpleInjectorInitializer.cs
+++ eGaragem.UI.Mvc/App_Start/SimpleInjectorInitializer.cs
@@ -1,13 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using eGaragem.Infra.CrossCutting.IoC;
+using eGaragem.UI.Mvc;
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
+using System.Reflection;
+using System.Web.Mvc;
 
 [assembly: WebActivator.PostApplicationStartMethod(typeof(SimpleInjectorInitializer), "Initialize")]
 
-namespace eGaragem.UI.Mvc.App_Start
+namespace eGaragem.UI.Mvc
 {
-    public class SimpleInjectorInitializer
+    public static class SimpleInjectorInitializer
     {
+        public static void Initialize()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+
+            InitializeContainer(container);
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+
+            container.Verify();
+
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+        private static void InitializeContainer(Container container)
+        {
+            BootStrapper.RegisterServices(container);
+        }
     }
 }
--- eGaragem.UI.Mvc/WebActivator/PostApplicationStartMethodAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WebActivator
-{
-    internal class PostApplicationStartMethodAttribute : Attribute
-    {
-    }
-}
